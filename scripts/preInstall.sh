55596f3b87e0144ab58ebc575fe8068ef71201af
jbenguira <z51biz@gmail.com>
2023-03-19 16:47:18 +0200

    fixes

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -20,3 +20,10 @@ docker-compose up -d;
 docker-compose up mongosetup;
 docker-compose up mongosetup
 
+#add auth
+echo "set auth activated"
+sed -i 's|"--bind_ip_all"|"--auth", "--keyFile", "/auth/key", "--bind_ip_all"|g' docker-compose.yml
+
+#stop the stack, will be restarted later with the auth added before
+docker-compose down;
+docker-compose up -d;
