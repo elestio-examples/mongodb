002aa83fe4df16c598686629718a89ca1ca0a2af
jbenguira <z51biz@gmail.com>
2023-03-19 16:09:43 +0200

    fixed order of tasks

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,15 @@
-#nothing to do
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#launch mongosetup a second time to ensure it's applied
+docker-compose up mongosetup;
+docker-compose up mongosetup
+
+#add auth
+echo "set auth activated"
+sed -i 's|"--bind_ip_all"|"--auth", "--keyFile", "/auth/key", "--bind_ip_all"|g' docker-compose.yml
+
+#stop the stack, will be restarted later with the auth added before
+docker-compose down;
+
+docker-compose up -d;
--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -13,16 +13,3 @@ echo $(openssl rand -hex 20) > ./auth/key;
 echo "set perm on Key file"
 chmod 600 ./auth/key 
 
-#activate the single node cluster and create admin account with mongosetup
-docker-compose up -d;
-
-#launch mongosetup a second time to ensure it's applied
-docker-compose up mongosetup;
-docker-compose up mongosetup
-
-#add auth
-echo "set auth activated"
-sed -i 's|"--bind_ip_all"|"--auth", "--keyFile", "/auth/key", "--bind_ip_all"|g' docker-compose.yml
-
-#stop the stack, will be restarted later with the auth added before
-docker-compose down;
