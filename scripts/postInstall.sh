9a2edd6ffd96eaeb84fac33f7c5da3b1f7a0cbe7
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-08 12:47:59 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,6 +1,14 @@
 #add auth
-echo "set auth activated"
-sed -i 's|"--bind_ip_all"|"--auth", "--keyFile", "/auth/key", "--bind_ip_all"|g' docker-compose.yml
+
+COMPOSE_FILE="docker-compose.yml"
+
+if grep -q '"--auth", "--keyFile"' "$COMPOSE_FILE"; then
+    echo "auth already activated"
+else
+    echo "set auth activated"
+    sed -i 's|"--bind_ip_all"|"--auth", "--keyFile", "/auth/key", "--bind_ip_all"|g' docker-compose.yml
+fi
+
 
 #stop the stack, will be restarted later with the auth added before
 docker-compose down;
