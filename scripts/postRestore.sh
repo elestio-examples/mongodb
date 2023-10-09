670091c3e1c7c8ab0b9a310c706781bde9a7da80
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-10-09 14:48:29 +0530

    Update postRestore.sh

--- scripts/postRestore.sh
+++ scripts/postRestore.sh
@@ -2,5 +2,8 @@
 docker-compose up -d;
 sleep 5s;
 
-#restore from last dump
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#restore from the last dump
 docker-compose exec -T mongo sh -c "mongorestore -u admin -p ${SOFTWARE_PASSWORD} --port 27017 --archive" < ./last.dump
