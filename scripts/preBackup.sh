71579936a8cad719ae3583bc09eb34aa656437d7
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-10-09 14:47:46 +0530

    Update preBackup.sh

--- scripts/preBackup.sh
+++ scripts/preBackup.sh
@@ -1,2 +1,5 @@
-#save latest dump to disk
-docker-compose exec -T mongo sh -c "mongodump -u admin -p [APP_PASSWORD] --port 27017 --archive" > ./last.dump
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#save the latest dump to disk
+docker-compose exec -T mongo sh -c "mongodump -u admin -p ${SOFTWARE_PASSWORD} --port 27017 --archive" > ./last.dump
